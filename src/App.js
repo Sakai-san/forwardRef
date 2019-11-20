7ac13b8038507e3059d200d71409343285f1a2a7
Sakai Toma <o_edo@hotmail.com>
2019-11-20 13:05:17 +0100

    Clean code.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class InnerBorder extends React.Component {
   constructor(props) {
-      super(props);
-​
-      this.ref = props.forwardedRef || React.createRef();
+    super(props);
+
+    this.ref = props.forwardedRef || React.createRef();
   }
-​
-  componentDidUpdate() {
-      console.log(
-          'InnerBorder',
-          'componentDidUpdate',
-          this.props.forwardedRef.current.getBoundingClientRect(),
-      );
+
+  componentDidMount() {
+    console.log(
+      "InnerBorder",
+      "componentDidUpdate",
+      this.props.forwardedRef.current.getBoundingClientRect()
+    );
   }
-​
+
   render() {
-      const { forwardedRef, ...rest } = this.props;
-​
-      return <div ref={forwardedRef} {...rest} style={{ border: '1px solid red' }} />;
+    const { forwardedRef, ...rest } = this.props;
+
+    return (
+      <div
+        ref={forwardedRef}
+        {...rest}
+        style={{ width: "80%", border: "1px solid red" }}
+      />
+    );
   }
 }
-​
+
 const EnhancedInnerBorder = React.forwardRef((props, ref) => (
   <InnerBorder {...props} forwardedRef={ref} />
 ));
-​
+
 class Wrapper extends React.Component {
   render() {
-      const { forwardedRef, ...rest } = this.props;
-​
-      return <EnhancedInnerBorder ref={forwardedRef} {...rest} />;
+    const { forwardedRef, ...rest } = this.props;
+
+    return <EnhancedInnerBorder ref={forwardedRef} {...rest} />;
   }
 }
-​
-const EnhancedWrapper = React.forwardRef((props, ref) => <Wrapper {...props} forwardedRef={ref} />);
-​
+
+const EnhancedWrapper = React.forwardRef((props, ref) => (
+  <Wrapper {...props} forwardedRef={ref} />
+));
+
 class Home extends React.Component {
   constructor(props) {
-      super(props);
-​
-      this.wrapperRef = React.createRef();
+    super(props);
+
+    this.wrapperRef = React.createRef();
   }
-​
-  componentDidUpdate() {
-      console.log('Home', 'componentDidUpdate', this.wrapperRef.current);
+
+  componentDidMount() {
+    console.log("Home", "componentDidMount", this.wrapperRef.current);
   }
-​
+
   render() {
-      return (
-          <React.Fragment>
-              <EnhancedWrapper ref={this.wrapperRef}>Bla</EnhancedWrapper>
-          </React.Fragment>
-      );
+    return (
+      <React.Fragment>
+        <EnhancedWrapper ref={this.wrapperRef}>Bla</EnhancedWrapper>
+      </React.Fragment>
+    );
   }
 }
+
 function App() {
   return (
     <div className="App">
