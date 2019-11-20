1701e387bb33eaca1008614d09cda3aeb3e5430f
Sakai Toma <o_edo@hotmail.com>
2019-11-20 11:39:53 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+class InnerBorder extends React.Component {
+  constructor(props) {
+      super(props);
+​
+      this.ref = props.forwardedRef || React.createRef();
+  }
+​
+  componentDidUpdate() {
+      console.log(
+          'InnerBorder',
+          'componentDidUpdate',
+          this.props.forwardedRef.current.getBoundingClientRect(),
+      );
+  }
+​
+  render() {
+      const { forwardedRef, ...rest } = this.props;
+​
+      return <div ref={forwardedRef} {...rest} style={{ border: '1px solid red' }} />;
+  }
+}
+​
+const EnhancedInnerBorder = React.forwardRef((props, ref) => (
+  <InnerBorder {...props} forwardedRef={ref} />
+));
+​
+class Wrapper extends React.Component {
+  render() {
+      const { forwardedRef, ...rest } = this.props;
+​
+      return <EnhancedInnerBorder ref={forwardedRef} {...rest} />;
+  }
+}
+​
+const EnhancedWrapper = React.forwardRef((props, ref) => <Wrapper {...props} forwardedRef={ref} />);
+​
+class Home extends React.Component {
+  constructor(props) {
+      super(props);
+​
+      this.wrapperRef = React.createRef();
+  }
+​
+  componentDidUpdate() {
+      console.log('Home', 'componentDidUpdate', this.wrapperRef.current);
+  }
+​
+  render() {
+      return (
+          <React.Fragment>
+              <EnhancedWrapper ref={this.wrapperRef}>Bla</EnhancedWrapper>
+          </React.Fragment>
+      );
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
